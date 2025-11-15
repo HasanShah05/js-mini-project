caeadd434b48e6e92bdda23b4b730a06cee745a8
ShahHasan05 <shahhasanhanif@gmail.com>
2025-11-15 22:08:35 +0530

    modified dark mode

--- digital clock/script.js
+++ digital clock/script.js
@@ -1,6 +1,27 @@
 const clock = document.getElementById('clock')
+const lightMode = document.getElementById('light')
+const darkMode = document.getElementById('dark')
+
 
 setInterval(function(){
     let date = new Date()
     clock.innerHTML = date.toLocaleTimeString()
 }, 1000)
+
+lightMode.addEventListener('mouseover', e => {
+    lightMode.style.cursor = 'pointer'
+})
+
+darkMode.addEventListener('mouseover', e => {
+    darkMode.style.cursor = 'pointer'
+})
+
+lightMode.addEventListener('click', e => {
+    document.body.style.backgroundColor = '#cdc8c8ff'
+    document.body.style.color = 'black'
+})
+
+darkMode.addEventListener('click', e => {
+    document.body.style.backgroundColor = '#212121'
+    document.body.style.color = 'white'
+})
