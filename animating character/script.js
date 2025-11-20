fc913bd346faf300c83e2fdc75345551a81e35dc
ShahHasan05 <shahhasanhanif@gmail.com>
2025-11-20 18:48:42 +0530

    aniamting characters

---
+++ animating character/script.js
@@ -0,0 +1,38 @@
+function randomColor(){
+    let str = '0123456789abcdef'
+    let hexColor = [ ]
+    for (let i = 0; i < 6; i++) {
+        let index = Math.floor(Math.random() * str.length)
+        hexColor.push(str[index])
+    }
+    return '#' + hexColor.join('')
+}
+
+function fontWeight(){
+    const weight = [1000,200,300,400,500]
+    return weight[Math.floor(Math.random() * weight.length)]
+}
+
+setInterval(() => {
+    const container = document.querySelector('div')
+    container.innerHTML = ''
+
+    const heading = document.createElement('h1')
+    const text = '30 days of javascript'.split('')
+
+    text.forEach(character => {
+        const span = document.createElement('span')
+        span.style.color = randomColor()
+        span.textContent = character;
+        span.style.letterSpacing = '5px'
+        span.style.fontSize = '8rem'
+        heading.appendChild(span)
+    })
+
+    heading.style.fontWeight = fontWeight();
+    container.style.backgroundColor = randomColor()
+
+
+    container.appendChild(heading)
+
+},2000)
